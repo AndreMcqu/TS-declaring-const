caad0251922e4566e3adf0db8c125e2748f6958c
andremcqu <mcquade.andre@gmail.com>
2022-05-23 09:52:58 +0200

    I declared two const and read them

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const name: string = "Dave"
+const age: Number = 42
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{name} is {age}</Text>
     </View>
   );
 }
